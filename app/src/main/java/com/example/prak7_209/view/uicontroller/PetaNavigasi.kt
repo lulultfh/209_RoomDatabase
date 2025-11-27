47184e85b6e5d8a49c6f591f7dbf2947248520de
lululthfh <punyalulu05@gmail.com>
2025-11-27 16:30:26 +0700

    feat: membuat function HostNavigasi yang digunakan sebagai pengatur halaman

---
+++ app/src/main/java/com/example/prak7_209/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,39 @@
+package com.example.prak7_209.view.uicontroller
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+//import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.prak7_209.view.EntrySiswaBody
+import com.example.prak7_209.view.EntrySiswaScreen
+import com.example.prak7_209.view.HomeScreen
+import com.example.prak7_209.view.route.DestinasiEntry
+import com.example.prak7_209.view.route.DestinasiHome
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController=navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(route = DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(route = DestinasiEntry.route)}
+            )
+        }
+        composable(route = DestinasiEntry.route){
+            EntrySiswaScreen(
+                navigateBack = {navController.popBackStack()}
+            )
+        }
+    }
+}
