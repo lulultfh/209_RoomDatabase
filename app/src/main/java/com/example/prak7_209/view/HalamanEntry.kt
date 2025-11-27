e7107065dd189fe6a3ff90f034f71604a250f243
lululthfh <punyalulu05@gmail.com>
2025-11-27 16:00:16 +0700

    feat: menambahkan function EntrySIswaScreen yang memiliki parameter navigateBack, modifier dan viewModel EntryViewModel, memiliki Scaffold yang memiliki paramter topBar dengan memanggil function SiswaTopAppBar dan body dengan memanggil EntrySiswaBody

--- app/src/main/java/com/example/prak7_209/view/HalamanEntry.kt
+++ app/src/main/java/com/example/prak7_209/view/HalamanEntry.kt
@@ -32,6 +32,41 @@ import kotlin.math.sin
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+fun EntrySiswaScreen(
+    navigateBack:() -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    )
+    { innerPadding ->
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+@Composable
 fun EntrySiswaBody(
     uiStateSiswa: UIStateSiswa,
     onSiswaValueChange: (DetailSiswa)-> Unit,
