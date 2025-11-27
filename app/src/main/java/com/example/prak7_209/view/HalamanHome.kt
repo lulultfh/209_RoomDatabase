150d50e9f149e1d3763c065381054106bc914544
lululthfh <punyalulu05@gmail.com>
2025-11-27 15:33:04 +0700

    feat: menambahkan function Composable HomeScreen yang memiliki parameter navigateToItemEntry, modifier dan viewModel HomeViewModel, didalamnya terdapat Scaffold yang memiliki parameter topBar dengan memanggil function SiswaTopAppBar dan floatingActionButton yang mengarahkan kepada navigateToItemEntry, Scaffold ini memiliki body dengan memanggil function BodyHome untuk menampilkan listSiswa

--- app/src/main/java/com/example/prak7_209/view/HalamanHome.kt
+++ app/src/main/java/com/example/prak7_209/view/HalamanHome.kt
@@ -41,6 +41,43 @@ import com.example.prak7_209.viewmodel.provider.PenyediaViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiHome.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.entry_siswa)
+                )
+            }
+        }) { innerPadding ->
+        val uiStateSiswa by viewModel.homeUiState.collectAsState()
+        BodyHome(
+            itemSiswa = uiStateSiswa.listSiswa,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        )
+    }
+}
+@Composable
 fun BodyHome(
     itemSiswa: List<Siswa>,
     modifier: Modifier = Modifier
