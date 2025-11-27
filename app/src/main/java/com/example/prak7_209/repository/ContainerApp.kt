8c2c6ece895bd69eaf00cef1b572faae7b9ffd95
lululthfh <punyalulu05@gmail.com>
2025-11-27 11:40:31 +0700

    feat: membuat interface ContainerApp yang mendeklarasikan properties RepositoriSIswa

---
+++ app/src/main/java/com/example/prak7_209/repository/ContainerApp.kt
@@ -0,0 +1,10 @@
+package com.example.prak7_209.repository
+
+import android.app.Application
+import android.content.Context
+import com.example.prak7_209.room.DatabaseSiswa
+
+interface ContainerApp{
+    val RepositoriSiswa : RepositoriSiswa
+}
+
