897d383f720c5b8f630d3241d43d3186229180b6
lululthfh <punyalulu05@gmail.com>
2025-11-27 12:16:55 +0700

    feat: menambahkan function untuk mengonversi dari Siswa ke DetailSiswa agar database dapat ditampilkan dalam UI

--- app/src/main/java/com/example/prak7_209/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/prak7_209/viewmodel/EntryViewModel.kt
@@ -23,3 +23,10 @@ fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateS
     detailSiswa = this.toDetailSiswa(),
     isEntryValid = isEntryValid
 )
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telepon
+)
