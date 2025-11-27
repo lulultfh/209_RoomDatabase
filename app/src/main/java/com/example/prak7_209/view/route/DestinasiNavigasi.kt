3f2709009fe46e28b1788890b9d0648f23f758a2
lululthfh <punyalulu05@gmail.com>
2025-11-27 16:18:01 +0700

    feat: mendeklarasikan interface DestinasiNavigasi untuk menyetandari (as template) route dan title pada setiap navigasi

---
+++ app/src/main/java/com/example/prak7_209/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,8 @@
+package com.example.prak7_209.view.route
+
+interface DestinasiNavigasi{
+    //nama unik utk menentukan jalur utk composable
+    val route: String
+//    string resource id yang berisi judul yang akan ditampilkan di layar halaman
+    val titleRes: Int
+}
