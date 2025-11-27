7ecb9c036580dd2c37aeac53f6c45f2951dc085b
lululthfh <punyalulu05@gmail.com>
2025-11-27 16:33:51 +0700

    feat: memanggil function SiswaApp pada body Scaffold pada MainActivity.kt

--- app/src/main/java/com/example/prak7_209/MainActivity.kt
+++ app/src/main/java/com/example/prak7_209/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.prak7_209.ui.theme.Prak7_209Theme
+import com.example.prak7_209.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Prak7_209Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
