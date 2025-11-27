9f2343a9ba9268ad49739b00abc2e25699aaa922
lululthfh <punyalulu05@gmail.com>
2025-11-27 14:48:02 +0700

    feat: mengimplementasikan PenyediaViewModel dengan factory untuk menyediakan viewModel pada PenyediaViewModel.kt

--- app/src/main/java/com/example/prak7_209/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak7_209/viewmodel/provider/PenyediaViewModel.kt
@@ -5,7 +5,19 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.prak7_209.repository.AplikasiSiswa
+import com.example.prak7_209.viewmodel.EntryViewModel
 import com.example.prak7_209.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
 
 fun CreationExtras.aplikasiSiswa(): AplikasiSiswa=
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as AplikasiSiswa)
