110e273d24632b75718abba3bf97abdfe941e953
lululthfh <punyalulu05@gmail.com>
2025-11-27 12:02:27 +0700

    feat: menambahkan function DetailSiswa pada EntryViewModel.kt yang berfungsi untuk mengonversi data input ke dalam tabel sesuai dengan jenis data

--- app/src/main/java/com/example/prak7_209/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/prak7_209/viewmodel/EntryViewModel.kt
@@ -3,10 +3,18 @@ package com.example.prak7_209.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.prak7_209.repository.RepositoriSiswa
+import com.example.prak7_209.room.Siswa
 
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
     val alamat: String = "",
     val telepon: String = ""
+)
+
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telepon
 )
