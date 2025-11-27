39938a1cafb0ba445375dc809100eeff2c09146a
lululthfh <punyalulu05@gmail.com>
2025-11-27 16:25:08 +0700

    feat: menambahkan pengimplementasian dari interface DestinasiNavigasi yakni DestinasiEntry untuk mendefinisikan route item_entry dan title resource yang mengambil dari String entry_siswa

---
+++ app/src/main/java/com/example/prak7_209/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.prak7_209.view.route
+
+import com.example.prak7_209.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
