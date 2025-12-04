f4b9645e970212734d60a4142358017087e6d00d
lululthfh <punyalulu05@gmail.com>
2025-12-04 12:07:03 +0700

    fix: memperbaiki typo (menambahkan tanda '}') pada DestinasiEditSiswa.kt

--- app/src/main/java/com/example/prak7_209/view/route/DestinasiEditSiswa.kt
+++ app/src/main/java/com/example/prak7_209/view/route/DestinasiEditSiswa.kt
@@ -6,5 +6,5 @@ object DestinasiEditSiswa : DestinasiNavigasi {
     override val route = "item_edit"
     override val titleRes = R.string.edit_siswa
     const val itemIdArg = "idSiswa"
-    val routeWithArgs = "$route/{$itemIdArg"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
