beafa75730ab88e3c5fc6e07178af619233dd81b
lululthfh <punyalulu05@gmail.com>
2025-12-04 11:21:37 +0700

    feat: menambahkan pengimplementasian dari interface DestinasiNavigasi yakni DestinasiDetailSiswa untuk mendefinisikan route detail_siswa, title resource yang diambil dari string resource detail siswa, dan argumen idSiswa beserta routeWithArgs untuk mendukung navigasi dengan parameter ID

---
+++ app/src/main/java/com/example/prak7_209/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.prak7_209.view.route
+
+import com.example.prak7_209.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
