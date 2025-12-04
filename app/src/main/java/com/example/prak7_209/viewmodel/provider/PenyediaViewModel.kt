c8065fc670d55a11f52499e26875a8943d997729
lululthfh <punyalulu05@gmail.com>
2025-12-04 11:11:18 +0700

    feat: menginisialisasikan EntryVIewModel dan DetailViewModel pada PenyediaViewModel.kt

--- app/src/main/java/com/example/prak7_209/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak7_209/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.prak7_209.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.prak7_209.repository.AplikasiSiswa
+import com.example.prak7_209.viewmodel.DetailViewModel
 import com.example.prak7_209.viewmodel.EntryViewModel
 import com.example.prak7_209.viewmodel.HomeViewModel
 
@@ -16,6 +18,13 @@ object PenyediaViewModel {
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+        //tambahan
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),aplikasiSiswa().container.repositoriSiswa)
+        }
+//        initializer {
+//            EditViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
+//        }
     }
 }
 
