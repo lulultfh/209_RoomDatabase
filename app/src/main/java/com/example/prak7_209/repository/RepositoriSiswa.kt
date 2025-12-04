08317c09bd2d20069cc4a5a1b993a048ac6e87c3
lululthfh <punyalulu05@gmail.com>
2025-12-04 11:25:10 +0700

    feat: menambahkan deklarasi suspend fun deleteSiswa dan updateSiswa pada interface RepositoriSIswa dan kemudian diimplementasikan pada class OfflineRepositoriSiswa

--- app/src/main/java/com/example/prak7_209/repository/RepositoriSiswa.kt
+++ app/src/main/java/com/example/prak7_209/repository/RepositoriSiswa.kt
@@ -8,8 +8,8 @@ interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
     fun getSiswaStream(id: Int): Flow<Siswa?>
-//    suspend fun deleteSiswa(siswa: Siswa)
-//    suspend fun updateSiswa(siswa: Siswa)
+    suspend fun deleteSiswa(siswa: Siswa)
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
 class OfflineRepositoriSiswa(
@@ -18,6 +18,6 @@ class OfflineRepositoriSiswa(
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
-//    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
-//    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
